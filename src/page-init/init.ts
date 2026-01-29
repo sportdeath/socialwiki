6c93c4e03b31ea5a25c202e896d6660f36c549b3
Theia Henderson <theia@mit.edu>
2026-01-28 20:40:07 -0500

    make download work regardless of origin

--- src/page-init/init.ts
+++ src/page-init/init.ts
@@ -1,11 +1,6 @@
 import importMap from "./import-map.json";
 import { GraffitiRpcClient } from "./rpc-client";
 
-const importScript = document.createElement("script");
-importScript.type = "importmap";
-importScript.textContent = JSON.stringify(importMap);
-document.head.append(importScript);
-
 declare global {
   interface Window {
     graffiti: typeof GraffitiRpcClient;
@@ -13,11 +8,19 @@ declare global {
 }
 
 if (window.top !== window) {
+  // If an iframe, inject the import map
+  const importScript = document.createElement("script");
+  importScript.type = "importmap";
+  importScript.textContent = JSON.stringify(importMap);
+  document.head.append(importScript);
+
+  // Then inject graffiti
   window.graffiti = GraffitiRpcClient;
 } else {
   // If we are the top level window, wrap the content in an iframe
   // and spin up the RPC "server".
   // This allows SocialWiki pages to work as standalone files.
+  const currentScriptSrc = (document.currentScript as HTMLScriptElement).src;
   window.addEventListener("DOMContentLoaded", () => {
     // Serialize the entire document
     const html = document.documentElement.outerHTML;
@@ -27,8 +30,8 @@ if (window.top !== window) {
     iframe.style.position = "fixed";
     iframe.style.top = "0";
     iframe.style.left = "0";
-    iframe.style.width = "100vw";
-    iframe.style.height = "100vh";
+    iframe.style.width = "100dvw";
+    iframe.style.height = "100dvh";
     iframe.style.border = "none";
     iframe.srcdoc = html;
 
@@ -40,7 +43,18 @@ if (window.top !== window) {
 
     // Add the init server script
     const script = document.createElement("script");
-    script.src = "http://localhost:5173/init-server.js";
+    const origin = new URL(currentScriptSrc).origin;
+    script.src = `${origin}/init-server.js`;
     document.head.append(script);
+
+    // Other defaults
+    const metaCharset = document.createElement("meta");
+    metaCharset.setAttribute("charset", "utf-8");
+    document.head.appendChild(metaCharset);
+    const metaViewport = document.createElement("meta");
+    metaViewport.name = "viewport";
+    metaViewport.content =
+      "width=device-width, initial-scale=1, shrink-to-fit=no";
+    document.head.appendChild(metaViewport);
   });
 }
